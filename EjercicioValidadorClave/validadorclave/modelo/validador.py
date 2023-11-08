bebfc82a2a275ded53a058e87d676acb5063e05f
ValenRincon <141295803+Jrincon709@users.noreply.github.com>
2023-11-08 00:01:27 -0500

    Subiendo los archivos

---
+++ EjercicioValidadorClave/validadorclave/modelo/validador.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+from EjercicioValidadorClave.validadorclave.modelo.errores import ReglaValidacionError, ReglaValidacionGanimedesError, ReglaValidacionCalistoError
+
+
+class ReglaValidacion(ABC):
+
+    def __init__(self, longitud_esperada: int):
+        self.longitud_esperada = longitud_esperada
+
+    def _validar_longitud(self, clave: str) -> bool:
+        if len(clave) <= self.longitud_esperada:
+            return False
+        else:
+            return True
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        for caracter in clave:
+            if caracter.isupper():
+                return True
+
+        return False
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        for caracter in clave:
+            if caracter.islower():
+                return True
+
+        return False
+
+    def _contiene_numero(self, clave: str) -> bool:
+        for caracter in clave:
+            if caracter.isdigit():
+                return True
+
+        return False
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+
+    def __init__(self):
+        super().__init__(longitud_esperada=8)
+
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        caracteres_especiales = ["@", "_", "#", "$", "%"]
+        for caracter in clave:
+            if caracter in caracteres_especiales:
+                return True
+            else:
+                return False
+
+    def es_valida(self, clave: str) -> bool:
+        if (
+                not self._validar_longitud (clave) or
+                not self._contiene_mayuscula (clave) or
+                not self._contiene_minuscula (clave) or
+                not self._contiene_numero (clave) or
+                not self.contiene_caracter_especial (clave)
+        ):
+            raise ReglaValidacionGanimedesError ("La clave no cumple con las reglas de validación de Ganimedes.")
+        return True
+
+
+class ReglaValidacionCalisto(ReglaValidacion):
+
+    def __init__(self):
+        super().__init__(longitud_esperada=6)
+
+    def contiene_calisto(self, clave: str) -> bool:
+        return clave.find("calisto") != -1
+
+    def es_valida(self, clave: str) -> bool:
+        if (
+                not self._validar_longitud (clave) or
+                not self._contiene_mayuscula (clave) or
+                not self._contiene_minuscula (clave) or
+                not self._contiene_numero (clave) or
+                not self.contiene_calisto (clave)
+        ):
+            raise ReglaValidacionCalistoError ("La clave no cumple con las reglas de validación de Calisto.")
+        return True
+
+
+class Validador:
+
+    def __init__(self, regla: ReglaValidacion):
+        self.regla = regla
+
+    def es_valida(self, clave: str) -> bool:
+        return self.regla.es_valida(clave)
